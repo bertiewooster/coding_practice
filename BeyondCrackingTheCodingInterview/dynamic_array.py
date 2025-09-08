4810e77070c475293ecf4d2abb57af18b5df4b8c
Jeremy Monat <jemonat@calalum.org>
2025-09-08 17:10:23 -0400

    Add DynamicArray.set() and .size()

--- BeyondCrackingTheCodingInterview/dynamic_array.py
+++ BeyondCrackingTheCodingInterview/dynamic_array.py
@@ -13,6 +13,15 @@ class DynamicArray:
         else:
             return self.fixed_array[i]
 
+    def set(self, i, x):
+        if i < 0 or i >= self._size:
+            raise IndexError("Index out of bounds")
+        else:
+            self.fixed_array[i] = x
+
+    def size(self):
+        return self._size
+
     def print_some_values(self):
         for index in [1, -10, 20]:
             try:
@@ -24,3 +33,9 @@ dynamic_array = DynamicArray()
 print(dynamic_array)
 
 dynamic_array.print_some_values()
+print(f"{dynamic_array.size()=}")
+
+dynamic_array.set(0, 2)
+dynamic_array.set(2, 6)
+dynamic_array.print_some_values()
+print(f"{dynamic_array.size()=}")
