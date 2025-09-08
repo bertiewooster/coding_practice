26d18630c488d39566757a0814304c5d7835af36
Jeremy Monat <jemonat@calalum.org>
2025-09-08 17:24:21 -0400

    Add DynamicArray.pop_back() and .append()

--- BeyondCrackingTheCodingInterview/dynamic_array.py
+++ BeyondCrackingTheCodingInterview/dynamic_array.py
@@ -1,6 +1,6 @@
 class DynamicArray:
     def __init__(self):
-        self.capacity = 10
+        self.capacity = 2
         self._size = 0
         self.fixed_array = [None] * self.capacity
 
@@ -21,9 +21,25 @@ class DynamicArray:
 
     def size(self):
         return self._size
+    
+    def append(self, x):
+        self._size += 1
+        if self._size > self.capacity:
+            # Double the capacity
+            old_capacity = self.capacity
+            self.capacity *= 2
+            fixed_array_new = [None] * self.capacity
+            fixed_array_new[:old_capacity] = self.fixed_array
+            self.fixed_array = fixed_array_new
+        self.fixed_array[self._size - 1] = x
+
+    def pop_back(self):
+        last_element = self.fixed_array[self._size - 1]
+        self._size -= 1
+        return last_element
 
     def print_some_values(self):
-        for index in [1, -10, 20]:
+        for index in [0, -10, 20]:
             try:
                 dynamic_array.get(index)
             except IndexError:
@@ -35,7 +51,14 @@ print(dynamic_array)
 dynamic_array.print_some_values()
 print(f"{dynamic_array.size()=}")
 
-dynamic_array.set(0, 2)
-dynamic_array.set(2, 6)
+dynamic_array.append(2)
+dynamic_array.append(4)
+dynamic_array.append(6)
+
+dynamic_array.set(0, 0)
+# dynamic_array.set(2, 6)
 dynamic_array.print_some_values()
+print(f"{dynamic_array.size()=}")
+
+print(f"{dynamic_array.pop_back()=}")
 print(f"{dynamic_array.size()=}")
