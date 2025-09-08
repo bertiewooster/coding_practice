3d4fc1b2fca9cc03121d98321a9318de70832ad4
Jeremy Monat <jemonat@calalum.org>
2025-09-08 17:06:22 -0400

    Add DynamicArray.get()

--- BeyondCrackingTheCodingInterview/dynamic_array.py
+++ BeyondCrackingTheCodingInterview/dynamic_array.py
@@ -1,11 +1,26 @@
 class DynamicArray:
     def __init__(self):
         self.capacity = 10
-        self.size = 0
+        self._size = 0
         self.fixed_array = [None] * self.capacity
 
     def __str__(self):
         return f"{self.fixed_array}"
 
+    def get(self, i):
+        if i < 0 or i >= self._size:
+            raise IndexError("Index out of bounds")
+        else:
+            return self.fixed_array[i]
+
+    def print_some_values(self):
+        for index in [1, -10, 20]:
+            try:
+                dynamic_array.get(index)
+            except IndexError:
+                print(f"Index {index} out of range")
+
 dynamic_array = DynamicArray()
 print(dynamic_array)
+
+dynamic_array.print_some_values()
