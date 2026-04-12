ca2e89c7b38ab215301af7bb5242c8b5281593c1
Jeremy Monat <jemonat@calalum.org>
2026-04-12 10:18:07 -0400

    Leetcode LRU my second time

--- LeetCode/LeetCode_146_LRU_Cache.py
+++ LeetCode/LeetCode_146_LRU_Cache.py
@@ -5,36 +5,27 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = dict()
-        self.lru:deque = deque()
+        self.lru = deque()
         pass
 
     def get(self, key: int) -> int:
-        print(f"Running get {key=}; {self.cache=} {self.lru=}")
         if key not in self.cache:
             return -1
-        # Remove key from lru dq
         self.lru.remove(key)
         self.lru.append(key)
         return self.cache[key]
         
 
     def put(self, key: int, value: int) -> None:
-        print(f"Running put {key=} {value=}; {self.cache=} {self.lru=}")
+        # print(f"Running put {key=} {value=}; {self.cache=} {self.lru=}")
+        if (key not in self.cache) and (len(self.cache) == self.capacity):
+            key_evict = self.lru.popleft()
+            # print(f"  Evict {key_evict=} from {self.cache=} and {self.lru=}")
+            del self.cache[key_evict]
         if key in self.cache:
-            pass
-        else:
-            if len(self.lru) == self.capacity:
-                key_evict = self.lru.popleft()
-                del self.cache[key_evict]
-        # If key already exists in lru, remove it
-        try:
             self.lru.remove(key)
-        except ValueError:
-            pass
-
         self.lru.append(key)
         self.cache[key] = value
-        print(f"  After: {self.cache=} {self.lru=}")
         return "null"
 
 # Your LRUCache object will be instantiated and called as such:
