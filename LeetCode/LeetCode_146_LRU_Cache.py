ba38f399b5f11714653fd38adf6120f2a0947796
Jeremy Monat <jemonat@calalum.org>
2026-04-12 10:42:08 -0400

    Leetcode LRU with OrderedDict

--- LeetCode/LeetCode_146_LRU_Cache.py
+++ LeetCode/LeetCode_146_LRU_Cache.py
@@ -1,32 +1,26 @@
-from collections import deque
+import collections
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = dict()
-        self.lru = deque()
-        pass
+        self.cache = collections.OrderedDict()
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        self.lru.remove(key)
-        self.lru.append(key)
+        self.cache.move_to_end(key)
         return self.cache[key]
         
 
     def put(self, key: int, value: int) -> None:
-        # print(f"Running put {key=} {value=}; {self.cache=} {self.lru=}")
-        if (key not in self.cache) and (len(self.cache) == self.capacity):
-            key_evict = self.lru.popleft()
-            # print(f"  Evict {key_evict=} from {self.cache=} and {self.lru=}")
-            del self.cache[key_evict]
         if key in self.cache:
-            self.lru.remove(key)
-        self.lru.append(key)
+            self.cache.move_to_end(key)
+        
         self.cache[key] = value
-        return "null"
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
