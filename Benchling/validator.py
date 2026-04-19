9cd685252e078dab1eca209a1a924fd7ce08ab08
Jeremy Monat <jemonat@calalum.org>
2026-04-19 13:22:00 -0400

    Use ValidationError dataclass

--- Benchling/validator.py
+++ Benchling/validator.py
@@ -1,4 +1,5 @@
 import re
+from dataclasses import dataclass
 
 # payload = {
 #     "name": "Sample A",
@@ -88,46 +89,40 @@ schema = {
 descr_class = {"boolean": bool, "integer": int, "string": str}
 
 
-def format_error(hierarchy, field_name, message):
-    full_hierarchy = hierarchy + [field_name]
-    full_message = f"{'.'.join(full_hierarchy)}: {message}"
-    return full_message
+@dataclass
+class ValidationError:
+    """Helper to format error messages"""
+
+    path: list
+    message: str
+
+    def __str__(self):
+        return f"{'.'.join(self.path)}: {self.message}"
 
 
 def check_required(payload, field_name, rules, hierarchy):
     errors = []
-    # Check if field is required
-    # Do this before checking is nested so that a required field won't crash
-    # by calling None.get(field_name) aka value.get(field_name)
+    path = hierarchy + [field_name]
     is_required = rules.get("required", False)
 
     # If field is required by schema and
     #   there was no payload passed in (value is None) (guard against NoneType not iterable error)
     #   or field isn't included
     if is_required and ((payload is None) or (field_name not in payload)):
-        errors.append(
-            format_error(
-                hierarchy=hierarchy,
-                field_name=field_name,
-                message="required field missing.",
-            )
-        )
+        error = ValidationError(path=path, message="required field missing.")
+        errors.append(error)
     return errors
 
 
 def check_nested(value, field_name, rules, hierarchy):
     errors = []
+    path = hierarchy + [field_name]
     # Not required and not present; skip silently
     if value is None:
         return errors
     if not isinstance(value, dict):
-        errors.append(
-            format_error(
-                hierarchy=hierarchy,
-                field_name=field_name,
-                message="expected an object",
-            )
-        )
+        error = ValidationError(path=path, message="expected an object")
+        errors.append(error)
         return errors
     errors_recursive = validate(
         payload=value,
@@ -140,79 +135,69 @@ def check_nested(value, field_name, rules, hierarchy):
 
 def check_type(field_name, value, hierarchy, cls_should_be, has_type):
     errors = []
+    path = hierarchy + [field_name]
 
     # Check field's (data) type
     if has_type:
         if type(value) is not cls_should_be:
-            errors.append(
-                format_error(
-                    hierarchy=hierarchy,
-                    field_name=field_name,
-                    message=f"expected {cls_should_be.__name__}, got {type(value).__name__}",
-                )
+            error = ValidationError(
+                path=path,
+                message=f"expected {cls_should_be.__name__}, got {type(value).__name__}",
             )
+            errors.append(error)
     return errors
 
 
 def check_integer(value, rules, hierarchy, field_name):
     errors = []
+    path = hierarchy + [field_name]
 
     # Check min
     if "min" in rules:
         min_val = rules.get("min", None)
         if min_val is not None:
             if value < min_val:
-                errors.append(
-                    format_error(
-                        hierarchy=hierarchy,
-                        field_name=field_name,
-                        message=f"{value} is below minimum {min_val}",
-                    )
+                error = ValidationError(
+                    path=path,
+                    message=f"{value} is below minimum {min_val}",
                 )
+                errors.append(error)
 
     # Check max
     if "max" in rules:
         max_val = rules.get("max", None)
         if max_val is not None:
             if value > max_val:
-                errors.append(
-                    format_error(
-                        hierarchy=hierarchy,
-                        field_name=field_name,
-                        message=f"{value} is above maximum {max_val}",
-                    )
+                error = ValidationError(
+                    path=path,
+                    message=f"{value} is above maximum {max_val}",
                 )
+                errors.append(error)
 
     return errors
 
 
 def check_string(value, rules, hierarchy, field_name):
     errors = []
+    path = hierarchy + [field_name]
     # Check min_length
     if "min_length" in rules:
         min_length = rules.get("min_length", None)
         if min_length is not None:
             if len(value) < min_length:
-                errors.append(
-                    format_error(
-                        hierarchy=hierarchy,
-                        field_name=field_name,
-                        message=f"Min length {min_length} not met",
-                    )
+                error = ValidationError(
+                    path=path,
+                    message=f"Min length {min_length} not met",
                 )
+                errors.append(error)
 
     # Check pattern
     if "pattern" in rules:
         pattern = rules.get("pattern", None)
         if pattern is not None:
             if not re.fullmatch(pattern=pattern, string=value):
-                errors.append(
-                    format_error(
-                        hierarchy=hierarchy,
-                        field_name=field_name,
-                        message=f"Pattern {pattern} not met",
-                    )
-                )
+                error = ValidationError(path=path, message=f"Pattern {pattern} not met")
+                errors.append(error)
     return errors
 
 
@@ -229,8 +214,12 @@ def validate(payload, schema, hierarchy=None):
         # Fetch this field from the payload
         value = payload.get(field_name) if payload else None
 
-        # Check if field required
-        required_error = check_required(payload, field_name, rules, hierarchy)
+        # Check if field is required
+        # Do this before checking is nested so that a required field won't crash
+        # by calling None.get(field_name) aka value.get(field_name)
+        required_error = check_required(
+            payload=payload, field_name=field_name, rules=rules, hierarchy=hierarchy
+        )
         if required_error:
             errors += required_error
             continue
@@ -257,5 +246,20 @@ def validate(payload, schema, hierarchy=None):
     return errors
 
 
-v = validate(payload=payload, schema=schema)
-print(v)
+errors = validate(payload=payload, schema=schema)
+
+# For display
+for error in errors:
+    print(error)
+
+# For JSON API response
+error: ValidationError
+json_api_response = []
+for error in errors:
+    json_api_response += [{"path": error.path, "message": error.message}]
+print(json_api_response)
+
+# For filtering
+address_errors = [error for error in errors if "address" in error.path]
+for error in address_errors:
+    print(error)
